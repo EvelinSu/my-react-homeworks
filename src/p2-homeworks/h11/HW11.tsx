b2b437f4efa1842ac6b8d968d664aceab0ddceaa
EvelinSu <firest-evi@mail.ru>
2022-09-25 16:36:38 +0300

    move to another file

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,37 +1,39 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from "../h4/HW4.module.css";
 
 function HW11() {
+
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
     return (
         <div>
-            <hr/>
-            homeworks 11
-
+            <h1>
+                homeworks 11
+            </h1>
             {/*should work (должно работать)*/}
-            <div>
+            <div className={s.column}>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={setValue1}
+                    value={value1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
-
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
+                   // onChangeRange={() => [setValue1, setValue2]}
+                    value={[value1, value2]}
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
             </div>
-
-            <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
             {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
         </div>
     )
 }
