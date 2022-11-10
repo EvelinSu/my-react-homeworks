c1dfec3c3ee7f3b9216f32ed4d0ec83bc0f2d7ed
EvelinSu <firest-evi@mail.ru>
2022-11-10 21:07:17 +0300

    - code refactoring

--- src/p2-homeworks/h2/Affairs/Affair.tsx
+++ src/p2-homeworks/h2/Affairs/Affair.tsx
@@ -1,34 +1,33 @@
 import React from "react";
-import { AffairType, FilterType } from "./types";
+import {AffairType, FilterType} from "./types";
 import {
-  SAffair,
-  SAffairButton,
-  SAffairPriority,
-  SAffairsButton,
+    SAffair,
+    SAffairButton,
+    SAffairPriority,
 } from "./styled";
 
 type AffairPropsType = {
-  affair: AffairType;
-  deleteAffairCallback: () => void;
-  activeFilter: FilterType;
+    affair: AffairType;
+    deleteAffairCallback: () => void;
+    activeFilter: FilterType;
 };
 
 function Affair(props: AffairPropsType) {
-  const deleteCallback = () => {
-    props.deleteAffairCallback();
-  };
-  return (
-    <SAffair>
-      <span>{props.affair.name}</span>
-      <SAffairPriority
-        activeFilter={props.activeFilter}
-        priority={props.affair.priority}
-      >
-        - {props.affair.priority} -
-      </SAffairPriority>
-      <SAffairButton onClick={deleteCallback}>x</SAffairButton>
-    </SAffair>
-  );
+    const deleteCallback = () => {
+        props.deleteAffairCallback();
+    };
+    return (
+        <SAffair>
+            <span>{props.affair.name}</span>
+            <SAffairPriority
+                activeFilter={props.activeFilter}
+                priority={props.affair.priority}
+            >
+                - {props.affair.priority} -
+            </SAffairPriority>
+            <SAffairButton onClick={deleteCallback}>x</SAffairButton>
+        </SAffair>
+    );
 }
 
 export default Affair;
